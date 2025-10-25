c98a50fb2d95494a1e8c17f1c865d1f756b155de
braynerdavid08-alt <braynerdavid08@gmail.com>
2025-10-24 19:11:02 -0500

    Proyecto spring boot braynner con java 21

---
+++ src/main/java/com/example/btictactoe/BtictactoeApplication.java
@@ -0,0 +1,13 @@
+package com.example.btictactoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtictactoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtictactoeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/btictactoe/BtictactoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.btictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtictactoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
